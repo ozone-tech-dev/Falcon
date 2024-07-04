2546e2a73a85b730842c9ef16638704716f33ca5
fitse01 <fitsumtafese01@gmail.com>
2024-07-04 20:08:50 +0300

    setup next js

---
+++ portfolio/src/app/page.js
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="flex justify-center font-bold text-4xl underline">
+      Hello
+    </div>
+  );
+}
